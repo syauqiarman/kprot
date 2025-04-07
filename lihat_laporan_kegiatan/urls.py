5476706d6f2577f5bb46c10181c4a03551f5df7c
razzlingbunny <alyandaastri19@gmail.com>
2025-04-07 16:22:35 +0700

    udh fungsional tp zero css

--- lihat_laporan_kegiatan/urls.py
+++ lihat_laporan_kegiatan/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from lihat_laporan_kegiatan.views import lihat_laporan_kegiatan, dashboard_penyelia_sementara
+from lihat_laporan_kegiatan.views import lihat_laporan_kegiatan, dashboard_penyelia_sementara, persetujuan_laporan
 
 app_name = 'lihat_laporan_kegiatan'
 
 urlpatterns = [
     path('penyelia/lihat/<str:program>/<int:mahasiswa_id>/', lihat_laporan_kegiatan, name='lihat_laporan_kegiatan'),
     path('penyelia/dashboard/', dashboard_penyelia_sementara, name='dashboard_penyelia_sementara'),
-    # path('persetujuan_laporan/', persetujuan_laporan, name='persetujuan_laporan'),
+    path('penyelia/persetujuan/<str:program>/<int:id_mahasiswa>/', persetujuan_laporan, name='persetujuan_laporan'),
 ]
--- lihat_laporan_kegiatan/views.py
+++ lihat_laporan_kegiatan/views.py
@@ -1,45 +1,8 @@
-# from django.shortcuts import render, get_object_or_404, redirect
-# from django.contrib.auth.decorators import login_required
-# from input_detil.models import LaporanKP, LaporanMBKM
-# from lihat_laporan_kegiatan.forms import PersetujuanLaporanForm
-
-# def get_latest_laporan(penyelia):
-#     """Mengambil laporan terbaru mahasiswa yang dibimbing oleh penyelia."""
-#     # laporan_kp = LaporanKP.objects.filter(mahasiswa__penyelia=penyelia).order_by('-tanggal_pengajuan')
-#     laporan_kp = LaporanKP.objects.filter(mahasiswa__penyelia=penyelia).order_by('-tanggal_pengajuan')
-#     laporan_mbkm = LaporanMBKM.objects.filter(mahasiswa__penyelia=penyelia).order_by('-tanggal_pengajuan')
-#     return {'laporan_kp': laporan_kp, 'laporan_mbkm': laporan_mbkm}
-
-# # @login_required
-# def daftar_laporan(request):
-#     penyelia = request.user.penyelia
-#     laporan = get_latest_laporan(penyelia)
-#     return render(request, 'laporan/daftar_laporan.html', {'laporan': laporan})
-
-# # @login_required
-# def persetujuan_laporan(request, laporan_id, jenis):
-#     if jenis == 'kp':
-#         laporan = get_object_or_404(LaporanKP, id=laporan_id)
-#     else:
-#         laporan = get_object_or_404(LaporanMBKM, id=laporan_id)
-
-#     if request.method == 'POST':
-#         form = PersetujuanLaporanForm(request.POST, instance=laporan)
-#         if form.is_valid():
-#             laporan = form.save(commit=False)
-#             if not laporan.disetujui_penyelia:
-#                 laporan.disetujui_dosen = False  # Jika ditolak penyelia, dosen tidak bisa menyetujui
-#             laporan.save()
-#             return redirect('daftar_laporan')
-#     else:
-#         form = PersetujuanLaporanForm(instance=laporan)
-
-#     return render(request, 'laporan/persetujuan_laporan.html', {'form': form, 'laporan': laporan})
-
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_GET, require_POST
 from input_detil.models import Penyelia, PendaftaranKP, PendaftaranMBKM, Laporan, LaporanKP, LaporanMBKM, Mahasiswa
 
 # Create your views here.
@@ -53,7 +16,7 @@ def lihat_laporan_kegiatan(request, program, mahasiswa_id):
         return JsonResponse({'error': 'Unauthorized'}, status=403)
     
     # fetch laporan
-    laporan = fetch_laporan(mahasiswa_id, program)
+    laporan = fetch_laporan(request, mahasiswa_id, program)
   
     # get pendaftaran
     pendaftaran = None
@@ -74,11 +37,14 @@ def lihat_laporan_kegiatan(request, program, mahasiswa_id):
 def is_penyelia(user):
     return Penyelia.objects.filter(user=user).exists()
 
-def fetch_laporan(mahasiswa_id, program):
+def fetch_laporan(request, mahasiswa_id, program):
     if program == 'kp':
         this_laporan = LaporanKP.objects.filter(pendaftaran__mahasiswa__id=mahasiswa_id).first()
     elif program == 'mbkm':
         this_laporan = LaporanMBKM.objects.filter(pendaftaran__mahasiswa__id=mahasiswa_id).first()
+    else:
+        messages.error(request, 'Jenis program tidak dikenali.')
+        return redirect('dashboard_penyelia_sementara')
     
     return this_laporan
 
@@ -98,5 +64,26 @@ def dashboard_penyelia_sementara(request):
 
     return render(request, "dashboard_penyelia_sementara.html", context)
 
-def persetujuan_laporan():
-    return None
+@require_POST
+def persetujuan_laporan(request, program, id_mahasiswa):
+    action = request.POST.get('action')
+    feedback = request.POST.get('feedback', '')
+    
+    laporan = fetch_laporan(request, id_mahasiswa, program)
+
+    if action == 'approve':
+        laporan.status_persetujuan_penyelia = 'Disetujui'
+        laporan.save()
+        messages.success(request, 'Laporan disetujui.')
+    elif action == 'reject':
+        if not feedback.strip():
+            messages.error(request, 'Feedback harus diisi saat menolak laporan.')
+            return redirect(request.META.get('HTTP_REFERER'))
+        laporan.status_persetujuan_penyelia = 'Ditolak'
+        laporan.feedback_penolakan_penyelia = feedback
+        laporan.save()
+        messages.success(request, 'Laporan ditolak dengan feedback.')
+    else:
+        messages.error(request, 'Aksi tidak valid.')
+
+    return redirect(request.META.get('HTTP_REFERER'))
